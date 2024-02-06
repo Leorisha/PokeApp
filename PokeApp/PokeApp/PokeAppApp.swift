91e09a3ea785895ff7c21f1b1630a7c2239e721e
Ana Neto <ateresa.neto@gmail.com>
2024-02-06 19:38:10 +0000

    add base project.

---
+++ PokeApp/PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by Ana Neto on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
